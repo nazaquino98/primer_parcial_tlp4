4628280d96001ced3e13176e6e0662e7cdd15b43
Nazarena Aquino <nazarenaayelen49@gmail.com>
2025-09-25 12:12:45 -0300

    modificacion del patron factory

--- src/factory.ts
+++ src/factory.ts
@@ -1,34 +1,60 @@
 abstract class Equipo2 {
-    constructor(
-        protected tipo: string,
-        protected nombre: string,
-        protected ram: string,
-        protected procesador: string
-    ) { }
+  constructor(
+    protected tipo: string,
+    protected nombre: string,
+    protected ram: string,
+    protected procesador: string
+  ) {}
 
 
-    abstract detalles(): string;
+  abstract detalles(): string;
 }
 
-class Servidor extends Equipo2 {
-    constructor(nombre: string, ram: string, procesador: string) {
-        super("Servidor", nombre, ram, procesador);
-    }
 
-    detalles(): string {
-        return `Tipo: ${this.tipo}, Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`;
-    }
+class Notebook extends Equipo2 {
+  constructor(nombre: string, ram: string, procesador: string) {
+    super("Notebook", nombre, ram, procesador);
+  }
+
+  detalles(): string {
+    return `Tipo: ${this.tipo}, Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`;
+  }
+}
+
+class Desktop extends Equipo2 {
+  constructor(nombre: string, ram: string, procesador: string) {
+    super("Desktop", nombre, ram, procesador);
+  }
+
+  detalles(): string {
+    return `Tipo: ${this.tipo}, Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`;
+  }
+}
+
+
+class Servidor extends Equipo2 {
+  constructor(nombre: string, ram: string, procesador: string) {
+    super("Servidor", nombre, ram, procesador);
+  }
+
+  detalles(): string {
+    return `Tipo: ${this.tipo}, Nombre: ${this.nombre}, RAM: ${this.ram}, Procesador: ${this.procesador}`;
+  }
 }
 
 class EquipoFactory {
-    crearEquipo(tipo: string, nombre: string, ram: string, procesador: string): Equipo2 {
-        switch (tipo) {
-            case "Servidor":
-                return new Servidor(nombre, ram, procesador);
-            default:
-                throw new Error(`Tipo de equipo no soportado: ${tipo}`);
-        }
+  crearEquipo(tipo: string, nombre: string, ram: string, procesador: string): Equipo2 {
+    switch (tipo) {
+      case "Notebook":
+        return new Notebook(nombre, ram, procesador);
+      case "Desktop":
+        return new Desktop(nombre, ram, procesador);
+      case "Servidor":
+        return new Servidor(nombre, ram, procesador);
+      default:
+        throw new Error(`Tipo de equipo no soportado: tipo`);
     }
+  }
 }
 
 export async function runFactory() {
